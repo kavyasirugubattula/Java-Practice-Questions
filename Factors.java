67fd99898c33ff3043050d48424f9edfaed71099
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-10 11:37:03 +0530

    First commit

---
+++ Factors.java
@@ -0,0 +1,10 @@
+public class Factors {
+    public static void main(String[] args) {
+        int a =9;
+        for(int i=1;i<=9;i++){
+            if(a%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
