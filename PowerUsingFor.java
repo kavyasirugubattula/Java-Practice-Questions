c47df2f7af4fc7e0af222388a625e1fde2c97db1
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-10 11:26:18 +0530

    First commit

---
+++ PowerUsingFor.java
@@ -0,0 +1,9 @@
+public class PowerUsingFor {
+    public static void main(String[] args){
+       int a = 3,b = 4,result =1;
+       for(int i =0;i<b;i++){
+        result *= a;
+       } 
+       System.out.println(result);
+    }
+}
