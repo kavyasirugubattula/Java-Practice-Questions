37d77c69181c5cab8f7fae1b19e42a05d7384f30
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-10 10:59:22 +0530

    First commit

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 1,a=5;
+        for(int i=2;i<=a;i++){
+        n = n*i;
+        }
+        System.out.println("The factorial of " + a + "is " + n );
+    }
+}
