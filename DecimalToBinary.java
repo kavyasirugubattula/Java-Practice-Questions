7a1c241233f103d5cd7d5f0e9573a8f81b2f5b6b
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-20 11:33:17 +0530

    First commit

---
+++ DecimalToBinary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+        int i=0;
+        int binary[] = new int[100];
+        while(a>0){
+            binary[i] = a%2;
+            a /= 2;
+            i++;
+        }
+        System.out.println("Binary value is: ");
+        for(int j=i-1;j>=0;j--){
+            System.out.print(binary[j]);
+        }
+    }
+}
