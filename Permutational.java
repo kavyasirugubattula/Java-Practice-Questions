f687352cb36221e2a045919237fb7bc54dbc66cf
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-21 21:16:23 +0530

    First commit

---
+++ Permutational.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Permutational {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        System.out.print("Enter a number: ");
+        int r=sc.nextInt();
+        int fact1=1,fact2,per;
+        for(int i=n;i>1;i=i-1){
+            fact1 = fact1*i;
+        }
+        int number = n-r;
+        fact2=1;
+        for(int i=number;i>1;i=i-1){
+            fact2 = fact2*i;
+        }
+        per=fact1/fact2;
+        System.out.println(per);
+    }
+}
