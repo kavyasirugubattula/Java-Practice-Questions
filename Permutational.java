b22597529a6297d19bde6d5c94989797c5582356
SIRUGUBATTULA KAVYA <kavyasirugubattula@gmail.com>
2025-03-21 21:18:04 +0530

    First commit

--- Permutational.java
+++ Permutational.java
@@ -7,12 +7,11 @@ public class Permutational {
         int n=sc.nextInt();
         System.out.print("Enter a number: ");
         int r=sc.nextInt();
-        int fact1=1,fact2,per;
+        int fact1=1,fact2=1,per;
         for(int i=n;i>1;i=i-1){
             fact1 = fact1*i;
         }
         int number = n-r;
-        fact2=1;
         for(int i=number;i>1;i=i-1){
             fact2 = fact2*i;
         }
